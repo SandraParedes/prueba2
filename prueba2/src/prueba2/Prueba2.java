deb23b108b97b35cd6b64873065f640abb534e08
Sandra Paredes Hernandez <4685866@alu.murciaeduca.es>
2018-04-11 13:04:41 +0200

    Commit mensaje 2

--- prueba2/src/prueba2/Prueba2.java
+++ prueba2/src/prueba2/Prueba2.java
@@ -14,7 +14,8 @@ public class Prueba2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
+        System.out.println("Prueba2");
     }
     
 }
